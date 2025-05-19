25dc9e7d642e08d9c96f21d355f2883745796ac5
Mohammad Sarip <sarip.mohammad13@gmail.com>
2025-05-19 20:21:59 +0800

    Fix: added makemigrations for attendance app

--- scripts/deploy_tasks.sh
+++ scripts/deploy_tasks.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 echo "🔁 Running Django migrations..."
+python manage.py makemigrations attendance
 python manage.py migrate --noinput
 
 echo "🎨 Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput
